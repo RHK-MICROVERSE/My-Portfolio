ae8945c4981cbab58c0c19936ebcce24891de8b9
kalbek <access2kaleb@gmail.com>
2023-03-07 10:54:12 +0300

    implement mobile menu displays on menu button click

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
 function controlMobileMenu() {
-    
+    const open = document.getElementById('mobile-nav')
+    const close = document.getElementById('close-menu')
+    function openMenu() {
+        const menu = document.querySelector('#mobile-menu')
+        menu.classList.remove('invisible')
+        menu.classList.add('visible')
+    }
+    function closeMenu() {
+        const menu = document.querySelector('#mobile-menu')
+        menu.classList.remove('visible')
+        menu.classList.add('invisible')
+    }
+    open.addEventListener('click', () => {
+        console.log("hey")
+        openMenu(); 
+    })
+    close.addEventListener('click', () => {
+        closeMenu();
+    })
 }
+controlMobileMenu();
